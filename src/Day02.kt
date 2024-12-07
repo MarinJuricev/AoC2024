50bd9ba34d5b4d6262325deb294bb6f56015bcaf
marinjuricev <mjuric00@fesb.hr>
2024-12-07 21:58:09 +0100

    chore: Cleanup the day 2 part 1 solution a tiny bit

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,59 +4,59 @@ fun main() {
     val testInput = readInput("Day02_test")
     val realInput = readInput("Day02")
 
-//    check(part1(testInput) == 2)
-//    part1(realInput).println()
+    check(part1(testInput) == 2)
+    part1(realInput).println()
 
-    check(part2(testInput) == 4)
+//    check(part2(testInput) == 4)
 //    part2(realInput).println()
 }
 
-fun part1(input: List<String>): Int {
-    val processedLines = input.map { it.split(" ").map(String::toInt) }
-    var result = 0
+fun part1(input: List<String>): Int = input
+    .map { it.split(" ").map(String::toInt) }
+    .map { it.processLine() }
+    .reduce { acc, i -> acc + i }
 
-    for (line in processedLines) {
-        var isIncrease = false
-        for ((index, i) in line.withIndex()) {
-            when (index) {
-                0 -> {
-                    isIncrease = i < line[1]
-                    val diff = abs(line[1] - i)
-                    if (diff.isInvalid()) break
-                }
+fun part2(input: List<String>): Int {
+    input
+        .map { it.split(" ").map(String::toInt) }
+        .map { it.processLine() }
+        .reduce { acc, i -> acc + i }
 
-                line.lastIndex -> result++
-                else -> {
-                    val prev = line[index - 1]
-                    val next = line[index + 1]
-                    val prevDiff = abs(prev - i)
-                    val nextDiff = abs(next - i)
-                    if (prevDiff.isInvalid() || nextDiff.isInvalid())
-                        break
+    return 0
+}
 
-                    if (isIncrease) {
-                        if (prev > i)
-                            break
-                        if (next < i)
-                            break
-                    } else {
-                        if (prev < i)
-                            break
-                        if (next > i)
-                            break
-                    }
+private fun List<Int>.processLine(): Int {
+    var isIncrease = false
+    for ((index, i) in withIndex()) {
+        when (index) {
+            0 -> {
+                isIncrease = i < get(1)
+                val diff = abs(get(1) - i)
+                if (diff.isInvalid()) return 0
+            }
+
+            lastIndex -> return 1
+            else -> {
+                val prev = get(index - 1)
+                val next = get(index + 1)
+                val prevDiff = abs(prev - i)
+                val nextDiff = abs(next - i)
+                if (prevDiff.isInvalid() || nextDiff.isInvalid()) return 0
+
+                if (isIncrease) {
+                    if (prev > i) return 0
+                    if (next < i) return 0
+                } else {
+                    if (prev < i) return 0
+                    if (next > i) return 0
                 }
             }
         }
     }
 
-    return result
-}
-
-fun part2(input: List<String>): Int {
-
     return 0
 }
+
 
 private fun Int.isInvalid() = this > MAX_DIFF || this == 0
 
